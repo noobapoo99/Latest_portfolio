011289f979a9b87144a17047ef548afcc96b76fe
noobapoo99 <apoorvtripathi99@gmail.com>
2024-09-02 03:00:21 +0530

    again

--- node_modules/three/examples/jsm/nodes/materials/LineBasicNodeMaterial.js
+++ node_modules/three/examples/jsm/nodes/materials/LineBasicNodeMaterial.js
@@ -1,4 +1,5 @@
-import NodeMaterial from './NodeMaterial.js';
+import NodeMaterial, { addNodeMaterial } from './NodeMaterial.js';
+
 import { LineBasicMaterial } from 'three';
 
 const defaultValues = new LineBasicMaterial();
@@ -14,36 +15,14 @@ class LineBasicNodeMaterial extends NodeMaterial {
 		this.lights = false;
 		this.normals = false;
 
-		this.colorNode = null;
-		this.opacityNode = null;
-
-		this.alphaTestNode = null;
-
-		this.lightNode = null;
-
-		this.positionNode = null;
-
 		this.setDefaultValues( defaultValues );
 
 		this.setValues( parameters );
 
 	}
 
-	copy( source ) {
-
-		this.colorNode = source.colorNode;
-		this.opacityNode = source.opacityNode;
-
-		this.alphaTestNode = source.alphaTestNode;
-
-		this.lightNode = source.lightNode;
-
-		this.positionNode = source.positionNode;
-
-		return super.copy( source );
-
-	}
-
 }
 
 export default LineBasicNodeMaterial;
+
+addNodeMaterial( 'LineBasicNodeMaterial', LineBasicNodeMaterial );
